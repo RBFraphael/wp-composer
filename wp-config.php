7b61399b5c7eaf46f48db1f20c2cc9fb8cd0f7cc
Raphael Batista Fontão <rbfraphael@gmail.com>
2022-02-02 01:40:30 -0300

    Updated configuration schema

--- wp-config.php
+++ wp-config.php
@@ -6,19 +6,6 @@ define("WP_SITEURL", WP_HOME."/cms" );
 define("WP_CONTENT_DIR", dirname(__FILE__)."/content");
 define("WP_CONTENT_URL", WP_HOME."/content");
 
-/**
- * Authentication unique keys and salts.
- * {@link https://api.wordpress.org/secret-key/1.1/salt/.
- */
-define("AUTH_KEY", "put your unique phrase here");
-define("SECURE_AUTH_KEY", "put your unique phrase here");
-define("LOGGED_IN_KEY", "put your unique phrase here");
-define("NONCE_KEY", "put your unique phrase here");
-define("AUTH_SALT", "put your unique phrase here");
-define("SECURE_AUTH_SALT", "put your unique phrase here");
-define("LOGGED_IN_SALT", "put your unique phrase here");
-define("NONCE_SALT", "put your unique phrase here");
-
 /** Absolute path to the WordPress directory. */
 if (!defined("ABSPATH") ) {
     define("ABSPATH", dirname(__FILE__)."/cms");
